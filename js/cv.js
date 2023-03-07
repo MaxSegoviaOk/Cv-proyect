f187ae6e07000b9e017f3082cbd780efedc40015
Maxi Segovia <segovia.it.ms@gmail.com>
2023-03-07 13:30:24 -0300

    Agregado de funcion Windows open

--- js/cv.js
+++ js/cv.js
@@ -1,4 +1,4 @@
-document.getElementById('boton').addEventListener('click', function(){
+document.getElementById('autor').addEventListener('click', function(){
     console.log("Mi primer Proyecto de Desarrollo Web Front-End");
     document.getElementById("autor").innerHTML = "Maxi Segovia"
 });
@@ -7,6 +7,22 @@ document.getElementById('boton_hora').addEventListener('click',function displayD
     document.getElementById("hora").innerHTML = Date();
 });
 
-document.getElementById('boton_fb').addEventListener('click', function newTab(){
-    window.open("https://www.facebook.com/j.maxi.segovia","_blank");
+document.getElementById('facebook').addEventListener('click', function newTab(){
+    window.open("https://www.facebook.com","_blank");
+});
+
+document.getElementById('instagram').addEventListener('click', function newTab(){
+    window.open("https://www.instagram.com","_blank");
+});
+
+document.getElementById('twitter').addEventListener('click', function newTab(){
+    window.open("https://www.twitter.com","_blank");
+});
+
+document.getElementById('linkedin').addEventListener('click', function newTab(){
+    window.open("https://ar.linkedin.com/","_blank");
+});
+
+document.getElementById('whatsapp').addEventListener('click', function newTab(){
+    window.open("https://web.whatsapp.com/","_blank");
 });
